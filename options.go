1874db85d9041f586d5983a05e66781387bd19ce
Mathias Rigaud <mathias.rigaud@rainbird.ai>
2024-07-23 08:33:08 +0100

    fixed options double header setter

--- options.go
+++ options.go
@@ -24,12 +24,9 @@ type QueryOption func(o queryOption) queryOption
 // AddHeaders assigns the headers to the queryOption struct
 func AddHeaders(headers http.Header) QueryOption {
 	return func(o queryOption) queryOption {
-		if headers != nil {
-			o.headers = headers.Clone()
-			for k, vals := range headers {
-				for _, v := range vals {
-					o.headers.Add(k, v)
-				}
+		for k, vals := range headers {
+			for _, v := range vals {
+				o.headers.Add(k, v)
 			}
 		}
 		return o
